e893c6959a3878725b49133fef5cc8f39e8e5739
amigustafson <ami.gustafson@icloud.com>
2019-10-23 12:10:46 +0200

    set up list ID for posting

--- app.js
+++ app.js
@@ -15,12 +15,27 @@ app.get("/", function(req, res){
 
 });
 
+
 app.post("/", function(req, res){
   var firstName = req.body.fname;
   var lastName = req.body.lname;
   var email = req.body.email;
+
+    axios.post("https://us20.api.mailchimp.com/3.0/lists/8ef059138f")
+    .then(function(response) {
+      console.log(response.statusCode);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+
 });
 
 app.listen(port, function(){
   console.log("Server is running on port " + port);
 });
+
+// 30f369bc1758a6163766051399f21cc9-us20
+
+//Audience ID
+//8ef059138f
