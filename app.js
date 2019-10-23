a912984290b5a87395286bae637a5aefce71d256
amigustafson <ami.gustafson@icloud.com>
2019-10-23 10:52:34 +0200

    setup get, post and signup form

---
+++ app.js
@@ -0,0 +1,27 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+
+});
+
+app.post("/", function(req, res){
+  var firstName = req.body.fname;
+  console.log(firstName);
+  var lastName = req.body.lname;
+  var email = req.body.email;
+});
+
+app.listen(port, function(){
+  console.log("Server is running on port " + port);
+});
